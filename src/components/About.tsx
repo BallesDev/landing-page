f272071fe8908eeb968736fe6f764a5efcb0311d
Matias <49315112+matiasict@users.noreply.github.com>
2025-02-03 23:26:13 -0300

    feat: update About and Statistics components with revised content and statistics

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,10 +3,7 @@ import pilot from "../assets/pilot.png";
 
 export const About = () => {
   return (
-    <section
-      id="about"
-      className="container py-24 sm:py-32"
-    >
+    <section id="about" className="container py-24 sm:py-32">
       <div className="bg-muted/50 border rounded-lg py-12">
         <div className="px-6 flex flex-col-reverse md:flex-row gap-8 md:gap-12">
           <img
@@ -18,16 +15,23 @@ export const About = () => {
             <div className="pb-6">
               <h2 className="text-3xl md:text-4xl font-bold">
                 <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
-                  About{" "}
+                  Who{" "}
                 </span>
-                Company
+                We Are
               </h2>
-              <p className="text-xl text-muted-foreground mt-4">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
-                amet, consectetur adipiscing elit.
+              <p className="text-l text-muted-foreground mt-4">
+                BallesDEV is a software development startup founded in Villa
+                Ballester, Buenos Aires, Argentina. Our name,{" "}
+                <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
+                  Balles
+                </span>
+                , comes from our place of origin, a reflection of where our
+                journey began. Over time, we have expanded our reach, delivering
+                cutting-edge technology solutions to businesses worldwide. We
+                specialize in designing, developing, and implementing custom
+                software solutions, helping companies enhance their digital
+                presence through cloud computing, web & mobile applications, and
+                software modernization.
               </p>
             </div>
 
--- src/components/Statistics.tsx
+++ src/components/Statistics.tsx
@@ -6,20 +6,20 @@ export const Statistics = () => {
 
   const stats: statsProps[] = [
     {
-      quantity: "2.7K+",
-      description: "Users",
+      quantity: "15+",
+      description: "Clients",
     },
     {
-      quantity: "1.8K+",
-      description: "Subscribers",
+      quantity: "25+",
+      description: "Projects Delivered",
     },
     {
-      quantity: "112",
-      description: "Downloads",
+      quantity: "150K+",
+      description: "Lines of Code",
     },
     {
-      quantity: "4",
-      description: "Products",
+      quantity: "3",
+      description: "Cloud Platforms",
     },
   ];
 
@@ -27,10 +27,7 @@ export const Statistics = () => {
     <section id="statistics">
       <div className="grid grid-cols-2 lg:grid-cols-4 gap-8">
         {stats.map(({ quantity, description }: statsProps) => (
-          <div
-            key={description}
-            className="space-y-2 text-center"
-          >
+          <div key={description} className="space-y-2 text-center">
             <h2 className="text-3xl sm:text-4xl font-bold ">{quantity}</h2>
             <p className="text-xl text-muted-foreground">{description}</p>
           </div>
