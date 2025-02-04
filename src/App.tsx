c05e499d7dc0df680ad933a4e8882a0b34fcdff4
Matias <49315112+matiasict@users.noreply.github.com>
2025-02-04 15:23:30 -0300

    feat: rename Features component to Technologies and update its content; remove unused Technologies component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { About } from "./components/About";
 import { Cta } from "./components/Cta";
 import { FAQ } from "./components/FAQ";
-import { Features } from "./components/Features";
 import { Footer } from "./components/Footer";
 import { Hero } from "./components/Hero";
 import { HowItWorks } from "./components/HowItWorks";
@@ -12,6 +11,7 @@ import { Services } from "./components/Services";
 import { Team } from "./components/Team";
 import { Testimonials } from "./components/Testimonials";
 import "./App.css";
+import { Technologies } from "./components/Features";
 
 function App() {
   return (
@@ -20,7 +20,7 @@ function App() {
       <Hero />
       <About />
       <HowItWorks />
-      <Features />
+      <Technologies />
       <Services />
       <Cta />
       <Testimonials />
